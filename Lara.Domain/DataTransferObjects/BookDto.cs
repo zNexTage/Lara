124dae23460956234504dc8e762f80de7bc11392
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-02-19 10:08:43 -0300

    Domain: Criado BookDto

---
+++ Lara.Domain/DataTransferObjects/BookDto.cs
@@ -0,0 +1,9 @@
+namespace Lara.Domain.DataTransferObjects;
+
+public class BookDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public List<string> Authors { get; set; } = default!;
+}
