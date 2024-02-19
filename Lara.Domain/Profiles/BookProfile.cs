b97039227e78e567ccd0e20f2d7fe353cc5e748d
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-02-19 10:42:27 -0300

    Domain: Mapeamento de BookDto para Book.

---
+++ Lara.Domain/Profiles/BookProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Lara.Domain.DataTransferObjects;
+using Lara.Domain.Entities;
+
+namespace Lara.Domain.Profiles;
+
+public class BookProfile : Profile
+{
+    public BookProfile()
+    {
+        CreateMap<BookDto, Book>();
+    }
+}
