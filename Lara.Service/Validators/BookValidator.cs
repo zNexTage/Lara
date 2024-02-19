e054f89a254629f7d5463a4c54494481b6b8cb2c
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-02-19 10:41:12 -0300

    Services: Validação dos atributos de Book

---
+++ Lara.Service/Validators/BookValidator.cs
@@ -0,0 +1,32 @@
+using FluentValidation;
+using Lara.Domain.DataTransferObjects;
+
+namespace Lara.Service.Validators;
+
+public class BookValidator : AbstractValidator<BookDto>
+{
+    public BookValidator()
+    {
+        RuleFor(book => book.Title)
+            .NotEmpty().WithMessage("Informe o título do livro")
+            .NotNull().WithMessage("Informe o título do livro")
+            .MaximumLength(100).WithMessage("O título deverá ter no máximo 100 caracteres");
+        
+        RuleFor(book => book.Publisher)
+            .NotEmpty().WithMessage("Informe a editora")
+            .NotNull().WithMessage("Informe a editora")
+            .MaximumLength(80).WithMessage("O título deverá ter no máximo 80 caracteres");
+
+        RuleFor(book => book.Image)
+            .NotEmpty().WithMessage("Informe a foto do livro")
+            .NotNull().WithMessage("Informe a foto do livro");
+
+        RuleForEach(book => book.Authors)
+            .NotEmpty().WithMessage("Informe o autor do livro")
+            .NotNull().WithMessage("Informe o autor do livro");
+        
+        RuleFor(book => book.Authors)
+            .NotEmpty().WithMessage("Informe o(s) autor(es) do livro")
+            .NotNull().WithMessage("Informe o(s) autor(es) do livro");
+    }
+}
