fdc659411c5a4603b7da6c73d2c9b9bf20730464
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-02-19 10:41:41 -0300

    Service: Criado class base para os services.

---
+++ Lara.Service/Service/BaseService.cs
@@ -0,0 +1,62 @@
+using AutoMapper;
+using FluentValidation;
+using Lara.Domain.Contracts;
+using Lara.Domain.Entities;
+
+namespace Lara.Service.Service;
+
+public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : BaseEntity
+{
+    private readonly IBaseRepository<TEntity> _repository;
+    private readonly IMapper _mapper; 
+
+    public BaseService(IBaseRepository<TEntity> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+    
+    public TEntity Add<TValidator, TEntityDto>(TEntityDto obj) where TValidator : AbstractValidator<TEntityDto>
+    {
+        Validate(obj, Activator.CreateInstance<TValidator>());
+
+        var entity = _mapper.Map<TEntity>(obj);
+        
+        _repository.Insert(entity);
+        
+        return entity;
+    }
+
+    public void Delete(int id)
+    {
+        _repository.Delete(id);
+    }
+
+    public IList<TEntity> Get()
+    {
+        return _repository.Select();
+    }
+
+    public TEntity GetById(int id)
+    {
+        return _repository.Select(id);
+    }
+
+    public TEntity Update<TValidator, TEntityDto>(TEntityDto obj) where TValidator : AbstractValidator<TEntityDto>
+    {
+        Validate(obj, Activator.CreateInstance<TValidator>());
+        var entity = _mapper.Map<TEntity>(obj);
+        
+        _repository.Update(entity);
+
+        return entity;
+    }
+    
+    private void Validate<TEntityDto>(TEntityDto obj, AbstractValidator<TEntityDto> validator)
+    {
+        if (obj == null)
+            throw new Exception("Registros não detectados!");
+
+        validator.ValidateAndThrow(obj);
+    }
+}
