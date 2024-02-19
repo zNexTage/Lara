026d3456630053437837da8162fdd20ffa9be86f
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-02-19 10:02:10 -0300

    Data: Criado BaseRepository
    Classe base para realização de um CRUD

---
+++ Lara.Data/Repository/BaseRepository.cs
@@ -0,0 +1,48 @@
+using Lara.Domain.Contracts;
+using Lara.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lara.Data.Repository;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+{
+    private readonly PgSqlContext _context;
+
+    public BaseRepository(PgSqlContext context)
+    {
+        _context = context;
+    }
+    
+    public void Insert(TEntity obj)
+    {
+        _context.Set<TEntity>().Add(obj);
+        _context.SaveChanges();
+    }
+
+    public void Update(TEntity obj)
+    {
+        _context.Entry(obj).State = EntityState.Modified;
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        _context.Set<TEntity>().Remove(Select(id));
+    }
+
+    public IList<TEntity> Select()
+    {
+        return _context.Set<TEntity>().ToList();
+    }
+
+    public TEntity Select(int id)
+    {
+        var entity = _context.Set<TEntity>().Find(id);
+        if (entity is null)
+        {
+            throw new Exception($"Não foi encontrado item com id {id}");
+        }
+
+        return entity;
+    }
+}
