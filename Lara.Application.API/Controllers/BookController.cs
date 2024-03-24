0d8ca6c21be324f5ab72c52482151618d3d49951
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-03-24 14:23:49 -0300

    API: adicionado política admin only para post, put e delete.
    Apenas administradores podem criar, editar e remover um livro.

--- Lara.Application.API/Controllers/BookController.cs
+++ Lara.Application.API/Controllers/BookController.cs
@@ -56,7 +56,7 @@ public class BookController : LaraControllerBase
     /// <response code="204">Livro foi removido com sucesso</response>
     /// <response code="404">Livro não localizado. Verifique o id</response>
     [HttpDelete]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "ADMIN")]
     public IActionResult Delete(int id)
     {
         try
@@ -79,7 +79,7 @@ public class BookController : LaraControllerBase
     /// <response code="201">Livro registrado com sucesso</response>
     /// <response code="400">Os dados informados estão inválidos</response>
     [HttpPost]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Authorize(AuthenticationSchemes = "Bearer", Policy = "AdminOnly" )]
     public IActionResult Create([FromBody] BookDto bookDto)
     {
         try
@@ -107,7 +107,7 @@ public class BookController : LaraControllerBase
     /// <response code="404">Livro não localizado. Verifique o id</response>
     /// <response code="400">Os dados informados estão inválidos</response>
     [HttpPut("{id}")]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "ADMIN")]
     public IActionResult Update(int id, [FromBody] BookDto bookDto)
     {
         try
